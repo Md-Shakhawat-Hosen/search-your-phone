d12bc2d58a79f1b674a1e1a9408899d13aff6bd4
Md-Shakhawat-Hosen <shakhawat.hosen.nsu@gmail.com>
2023-08-28 19:53:08 +0600

    added modal

--- api.js
+++ api.js
@@ -11,7 +11,7 @@ const loadPhone = async (inputText='13',isShow) => {
 
 
 const displayPhones = (phones,isShow) => {
-    console.log(phones.length)
+    // console.log(phones.length)
      
     const productContainer = document.getElementById('product-container');
 
@@ -36,7 +36,7 @@ const displayPhones = (phones,isShow) => {
         
 
         showBtn.classList.remove('hidden')
-        console.log(phones);
+        // console.log(phones);
 
     }
      else {
@@ -65,7 +65,8 @@ const displayPhones = (phones,isShow) => {
                 <p class="text-center">There are many variations of passages of available, but the majority have suffered</p>
                 <h2 class="text-center font-bold">$999</h2>
                 <div class="card-actions justify-center">
-                  <button class="btn btn-primary">Show Details</button>
+                  <button onclick="showDetails('${element.slug}')" class="btn btn-primary">Show Details</button>
+                  
                 </div>
               </div>
             </div>
@@ -110,6 +111,48 @@ const showAll = () =>{
   
     SearchLoader(true)
 }
+
+
+const showDetails = async (id) => {
+    const res = await fetch(
+      `https://openapi.programming-hero.com/api/phone/${id}`
+    );
+    const data = await res.json();
+    const info = data.data;
+    showDetailsDisplay(info);
+}
+
+
+
+const showDetailsDisplay = (phone) => {
+    show_details_modal.showModal();
+
+    // console.log(phone);
+
+    const DetailsContainer = document.getElementById("details-container");
+
+    DetailsContainer.textContent = ''
+
+    const div = document.createElement('div');
+    div.innerHTML = `
+     <img src="${phone.image}" alt="">
+       <h2 class="font-bold">${phone.name}</h2>
+       <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+       <h2><span class="font-bold">Storage :</span> ${phone?.mainFeatures?.storage}</h2>
+       <h2><span class="font-bold">Display Size :</span>${phone?.mainFeatures?.displaySize} </h2>
+       <h2><span class="font-bold">Chipset :</span> ${phone?.mainFeatures?.chipSet}</h2>
+       <h2><span class="font-bold">Memory :</span> ${phone?.mainFeatures?.memory}</h2>
+       <h2><span class="font-bold">Slug : </span>${phone?.slug} </h2>
+       <h2><span class="font-bold">Release data :</span> ${phone?.releaseDate}</h2>
+       <h2><span class="font-bold">Brand :</span> ${phone?.brand}</h2>
+       <h2><span class="font-bold">GPS : </span> ${phone?.others?.GPS}</h2>
+    
+    
+    `;
+
+    DetailsContainer.appendChild(div);
+}
+
 
 
 loadPhone()
